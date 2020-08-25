235ac16d83fc7cc34e4351a0737b4fecd41e7a5a
Radu Profeanu <profeanu.radu@yahoo.com>
2020-08-25 17:39:24 +0300

    adding homework2WhileEx4.java.java

--- src/homework2WhileEx4.java
+++ src/homework2WhileEx4.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class homework2WhileEx4 {
+    public static void main(String[] args) {
+
+        boolean var=false;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduceti prima latura a triunghiului :");
+        int primaLaturaATriunghiului = scanner.nextInt();
+        System.out.println("Introduceti a doua latura a triunghiului :");
+        int aDouaLaturaATriunghiului = scanner.nextInt();
+        System.out.println("Introduceti a treia latura a triunghiului :");
+        int aTreiaLaturaATriunghiului = scanner.nextInt();
+
+        while(var==false){
+            if(primaLaturaATriunghiului+aDouaLaturaATriunghiului>aTreiaLaturaATriunghiului&& primaLaturaATriunghiului+aTreiaLaturaATriunghiului>aDouaLaturaATriunghiului && aDouaLaturaATriunghiului+aTreiaLaturaATriunghiului>primaLaturaATriunghiului){
+                System.out.println("Acestea sunt laturile unui triunghi");
+                var=true;
+            }
+        }
+
+
+
+    }
 }
