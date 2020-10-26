3538d69d15c1cf7b6c6c674fca381076818d5e96
Radu Profeanu <profeanu.radu@yahoo.com>
2020-10-26 14:02:05 +0200

    adding exercise 1 of homework 10

--- src/homework10/Solution.java
+++ src/homework10/Solution.java
@@ -1,25 +1,40 @@
 package homework10;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Solution {
 
-    public static int divide(int x, int y){
+/*    public static int divide(int x, int y){
 
         try{
           int z= x/y;
+            System.out.println(z);
 
-        } catch (Exception e) {
+        } catch (ArithmeticException e) {
             e.printStackTrace();
         }
         return x/y;
-    }
+    }*/
 
     public static void main(String[] args) {
 
-       int z=divide(23,0);
-        System.out.println("Divide is"+z);
+        Scanner scan= new Scanner(System.in);
+        try{
+            int x=scan.nextInt();
+            int y=scan.nextInt();
+            System.out.println(x/y);
 
-        int w=divide(10,0);
-        System.out.println(w);
+        }catch ( ArithmeticException| InputMismatchException e){
+            if(e instanceof ArithmeticException){
+                System.out.println("java.lang.ArithmeticException: / by zero");
+            }else if(e instanceof InputMismatchException){
+                System.out.println("java.util.InputMismatchException");
+            }
+
+        }
+        scan.close();
+      // int z=divide(10,3);
 
 
     }
