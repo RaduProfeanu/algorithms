132ecf05859a97a20711851e0d636027b7229367
Radu Profeanu <profeanu.radu@yahoo.com>
2020-11-09 11:46:18 +0200

    adding 1st exercise of homework 11

--- src/homework11/HomeworkClass.java
+++ src/homework11/HomeworkClass.java
@@ -7,6 +7,7 @@ public class HomeworkClass {
         Scanner sc=new Scanner(System.in);
         String A=sc.next();
         String B=sc.next();
+
         System.out.println(A.length()+B.length());
         if (A.compareTo(B)<0){
             System.out.println("No");
@@ -16,5 +17,8 @@ public class HomeworkClass {
         A = A.substring(0,1).toUpperCase() + A.substring(1).toLowerCase();
         B = B.substring(0,1).toUpperCase() + B.substring(1).toLowerCase();
         System.out.println(A+" "+B);
+
+        String A1=A.substring(3,7);
+        System.out.println(A1);
     }
 }
