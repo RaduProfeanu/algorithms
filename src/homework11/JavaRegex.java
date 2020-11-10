a9a0b8ad46c03650ce2fd8dbcfd56804d1af3f85
Radu Profeanu <profeanu.radu@yahoo.com>
2020-11-10 12:46:17 +0200

    wrapping up Homework11

---
+++ src/homework11/JavaRegex.java
@@ -0,0 +1,21 @@
+package homework11;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class JavaRegex {
+
+    static class MyRegex {
+        String pattern = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+    }
+}
