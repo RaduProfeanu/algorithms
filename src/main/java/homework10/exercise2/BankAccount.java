cc8db9c33eb4a116c7211a32110ed7ca627675f1
Radu Profeanu <profeanu.radu@yahoo.com>
2021-02-07 19:55:27 +0200

    this is a test

--- src/main/java/homework10/exercise2/BankAccount.java
+++ src/main/java/homework10/exercise2/BankAccount.java
@@ -4,48 +4,14 @@ import java.util.List;
 
 public class BankAccount {
 
-    private String iban;
-
+    private String bankAccount;
     private int balance;
-
     private List<Card> attachedCardNumber;
 
-    public BankAccount(String iban, int balance) {
-        this.iban = iban;
-        //validare
+
+    public BankAccount(String bankAccount, int balance) {
+        this.bankAccount = bankAccount;
         this.balance = balance;
-    }
-
-    public void addMoney(int amount) {
-
-        if (amount <= 0) {
-            balance = 0;
-        } else {
-            balance = balance + amount;
-        }
-        System.out.println("Money in the account " + balance);
-    }
-
-    public void withdrawMoney(int amount) {
-
-        if (amount >= balance) {
-            throw new NotEnoughBalanceException("Please deposit money before withdrawl");
-        } else {
-            balance = balance - amount;
-        }
-        System.out.println("Money in the account " + balance);
-    }
-
-    public void attachCard(Card card) {
-        attachedCardNumber.add(card);
-    }
-
-    @Override
-    public String toString() {
-        return "BankAccount{" +
-                "bankAccount='" + iban + '\'' +
-                ", balance=" + balance +
-                '}';
     }
 
     public int getBalance() {
@@ -56,11 +22,46 @@ public class BankAccount {
         this.balance = balance;
     }
 
-    public String getIban() {
-        return iban;
+    public String getBankAccount() {
+        return bankAccount;
     }
 
-    public void setIban(String iban) {
-        this.iban = iban;
+    public void setBankAccount(String bankAccount) {
+        this.bankAccount = bankAccount;
+    }
+
+    public void addMoney(int amount){
+
+        if(amount<=0){
+            this.balance=0;
+        }else{
+            this.balance=balance+amount;
+        }
+        System.out.println("Money in the account "+balance);
+    }
+
+    public void withdrawMoney(int amount){
+
+        if (balance<=0||amount>=balance){
+            throw new ArithmeticException("Please deposit money before withdrawl");
+        }else{
+            this.balance=balance-amount;
+        }
+        System.out.println("Money in the account "+balance);
+    }
+
+    public void attachCard(String cardNumber){
+
+        String newattachedCard=new String();
+      //  attachedCardNumber.add(newattachedCard);
+
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "bankAccount='" + bankAccount + '\'' +
+                ", balance=" + balance +
+                '}';
     }
 }
---
+++ src/main/java/homework14/TestClass.java
@@ -0,0 +1,7 @@
+package homework14;
+
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("this is a test");
+    }
+}
